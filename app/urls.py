a21594a90d1d8c3b4e99cb42ea680bf415511199
Ignacio Serdá <ignserda@gmail.com>
2023-03-06 20:39:08 -0300

    Agregue un template con css y js

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from app import views
 
 urlpatterns = [
     path("", views.inicio),
     path("bd/<name>/<code>", views.template),
     path("show/", views.variables),
+    path("facha/", views.fachero)
 ]
